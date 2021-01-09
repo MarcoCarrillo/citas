26bf6aa4197499b5603e28dc33ce82de47da7ef5
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2021-01-09 15:11:39 -0600

    citas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react'
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    //Arreglo de citas
+    const [citas, guardarCitas] = useState([]);
+
+    //Funcion que tome las citas actuales y agregue la nueva
+    const crearCita = cita => {
+        guardarCitas([
+            ...citas,
+            cita
+        ]);
+    }
+
+    //Funcion que elimina una cita por su id
+    const eliminarCita = id => {
+        const nuevasCitas= citas.filter(cita => cita.id !== id);
+        guardarCitas(nuevasCitas);
+    }
+
+
+
+    return ( 
+        <Fragment>
+        <h1> Administrador de Pacientes</h1>
+
+        <div className="container">
+            <div className="row">
+                <div className="one-half column">
+                    <Formulario
+                        crearCita={crearCita}
+                    />
+                </div>
+                <div className="one-half column">
+                    <h2>Administra tus citas</h2>
+                    {citas.map(cita => (
+                        <Cita
+                        key={cita.id}
+                        cita={cita}
+                        eliminarCita={eliminarCita}   
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+        </Fragment>
+    );
 }
 
 export default App;
